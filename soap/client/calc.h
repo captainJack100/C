372931bfa7263585706351c88b293a2d8fc3914a
captainjack <captain@jack.com>
2014-10-09 06:51:46 -0700

    getting ride of soap

--- soap/client/calc.h
+++
@@ -1,7 +0,0 @@
-//gsoap ns service name: calc
-//gsoap ns service namespace: http://www.mysite.com/calc.wsdl
-//gsoap ns service location: http://www.mysite.com/calc.cgi
-//gsoap ns schema namespace: urn:calc  
-int ns__add(double a, double b, double *result);  
-int ns__sub(double a, double b, double *result);  
-int ns__sqrt(double a, double *result); 
