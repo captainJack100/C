52e9ae3beb4513dc53ac86a1f8f049792b7e6e3a
captainjack <captain@jack.com>
2015-04-13 10:43:41 -0700

    get pid

---
+++ getPid.h
@@ -0,0 +1,28 @@
+/*
+ * Use for diagnostics. Grabs PID of a process.
+ *
+ *
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* get_process_name_by_pid(const int pid)
+{
+    char* name = (char*)calloc(1024,sizeof(char));
+    if(name){
+        sprintf(name, "/proc/%d/cmdline",pid);
+        FILE* f = fopen(name,"r");
+        if(f){
+            size_t size;
+            size = fread(name, sizeof(char), 1024, f);
+            if(size>0){
+                if('\n'==name[size-1])
+                    name[size-1]='\0';
+            }
+            fclose(f);
+        }
+    }
+    return name;
+}
+
